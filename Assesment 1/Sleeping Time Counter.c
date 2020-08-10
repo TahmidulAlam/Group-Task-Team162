0824954ac3ab26ce405eb97e53cf20c6148e8b72
Mahbub-Rafi <iftekher.mahbub@northsouth.edu>
2020-08-10 10:58:48 +0600

    Whille loop added;Four zero input will terminate programe

--- Assesment 1/Sleeping Time Counter.c
+++ Assesment 1/Sleeping Time Counter.c
@@ -2,17 +2,21 @@
 int main()
 {
     int h1,m1,h2,m2,result;
+    printf("Enter current hour and minute & then ringing hour and minute:\n");
+    while(1) //for running into infinity loop.
+    {
+       scanf("%d %d %d %d", &h1 , &m1 , &h2 , &m2); //Input can also be taken putting spaces between.
+       if(h1==0&&m1==0&&h2==0&&m2==0)
+            break; //four zero input will terminated the programe.
+       else if((h1>=00&&h1<=23)&&(m1>=00&&m1<=59)&&(h2>=00&&h2<=23)&&(m2>=00&&m2<=59))
+        {
+            result=(h2*60+m2-h1*60-m1+24*60)%(24*60);
+            printf("Remaining time is: %d\n\n", result); //double \n for 2 new lines.
+        }
 
-    scanf("%d %d %d %d", &h1 , &m1 , &h2 , &m2);
+        else
+            printf("Invalid time");
+    }
 
-    if((h1>=00&&h1<=23)&&(m1>=00&&m1<=59)&&(h2>=00&&h2<=23)&&(m2>=00&&m2<=59))
-      {
-      result=(h2*60+m2-h1*60-m1+24*60)%(24*60);
-      printf("%d", result);
-      }
-
-    else
-    printf("Invalid time");
-
-return 0;
+    return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Group-Task-Team162
+
+By - Mahbub-Rafi
+{
+ 1. while loop added for running the programes infinity times.
+ 2. Input four zeros will terminate the programme.
+ 3. Added a screenshot.
+
+ Note: Input can also be taken with putting spaces between the integers.
+}
